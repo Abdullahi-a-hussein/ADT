536b438066ce98fcbe6392785f24d99173fa2688
Abdullahi-a-hussein <abdullahi.a.hussein@gmail.com>
2021-09-10 14:13:36 -0400

    This commit is for creation of files

---
+++ Stack/stack.py
@@ -0,0 +1,7 @@
+"""
+This file contains the implementation code for a simple first-in last-out ADT (stack) classes:
+ -Regular stack class.
+ - Guarded Stack class
+----Attributes---
+        _container: List of objects
+"""
